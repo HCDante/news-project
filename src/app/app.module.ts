0d8d578a316865bd71968a93ef68f2fbd7babca2
hcdante <hectorhugoandradelinares@gmail.com>
2023-08-17 14:09:57 -0600

    Base project added.

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FeedComponent } from './components/feed/feed.component';
+import { ArticleComponent } from './components/article/article.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FeedComponent,
+    ArticleComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
