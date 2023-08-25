cdcdaa3d13cff43fe98942693f336444aa887b43
hcdante <hectorhugoandradelinares@gmail.com>
2023-08-25 14:41:45 -0600

    Added routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FeedComponent } from './components/feed/feed.component';
+import { ArticleComponent } from './components/article/article.component';
+import { AboutComponent } from './components/about/about.component';
 
 const routes: Routes = [
+  {path:'', component:FeedComponent},
+  {path:'article', component:ArticleComponent},
+  {path:'about', component:AboutComponent}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+
+  imports: [
+    RouterModule.forRoot(routes)
+
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
