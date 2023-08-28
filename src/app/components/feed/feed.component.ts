df2677b78c0cb494b7c8c93bd422eee173bff227
hcdante <hectorhugoandradelinares@gmail.com>
2023-08-28 13:37:55 -0600

    Async Pipe.

--- src/app/components/feed/feed.component.ts
+++ src/app/components/feed/feed.component.ts
@@ -3,6 +3,7 @@ import { NewsService } from '../../services/news.service';
 import { Observer } from 'rxjs';
 import { Article } from 'src/app/models/article.model';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-feed',
@@ -10,61 +11,84 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./feed.component.scss'],
 })
 
-
 export class FeedComponent implements OnInit {
-  noticias: Article[] = [];
-  noticiasPrincipales: Article[] = [];
-  noticiasSecundarias: Article[] = [];
+  noticias$: Observable<{ articles: Article[] }> | undefined;
 
-    constructor(
-      private route: ActivatedRoute,
-      private newsService: NewsService ) { }
+  constructor(private newsService: NewsService, private route: ActivatedRoute) {}
 
-    ngOnInit(): void {
-      this.route.paramMap.subscribe(params => {
-        const selecionarCategoria = params.get('categoria');
-      this.obtenerNoticias(selecionarCategoria?? 'general');
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      const selectedCategory = params['category'];
+      this.noticias$ = this.newsService.obtenerNoticias(selectedCategory, 'US');
+      console.log('Noticias', this.noticias$);
     });
   }
-
-    obtenerNoticias(selecionarCategoria: string): void {
-      // const categoria = 'general'; // Cambia a la categoría deseada
-      const pais = 'US'; // Cambia al país deseado
-
-      const observer$: Observer<any> = {
-        next: (response) => {
-          this.noticias = response.articles;
-          console.log('Noticias',this.noticias);
-
-          this.noticias.forEach((element, index) => {
-              if(index < 10){
-                this.noticiasPrincipales.push(element)
-              }
-              else{
-                this.noticiasSecundarias.push(element)
-              }
-          });
-
-        },
-        error: (error) => {
-          console.error(error);
-        },
-        complete: () => {
-          console.log('Completado');
-        }
-      }
-
-      this.newsService.obtenerNoticias(selecionarCategoria, pais).subscribe(observer$);
-
-      // this.newsService.obtenerNoticias(categoria, pais).subscribe(
-      //   (response) => {
-      //     this.noticias = response.articles;
-      //     console.log(this.noticias);
-      //   },
-      //   (error) => {
-      //     console.error(error);
-      //   }
-      // );
-    }
-
 }
+
+
+// export class FeedComponent implements OnInit {
+//   noticias: Article[] = [];
+//   noticiasPrincipales: Article[] = [];
+//   noticiasSecundarias: Article[] = [];
+
+//     constructor(
+//       private route: ActivatedRoute,
+//       private newsService: NewsService ) { }
+
+//     ngOnInit(): void {
+//       this.route.paramMap.subscribe(params => {
+//         const selecionarCategoria = params.get('categoria');
+//       this.obtenerNoticias(selecionarCategoria?? 'general');
+//     });
+//   }
+
+//     obtenerNoticias(selecionarCategoria: string): void {
+//       // const categoria = 'general'; // Cambia a la categoría deseada
+//       const pais = 'US'; // Cambia al país deseado
+
+//       const observer$: Observer<any> = {
+//         next: (response) => {
+//           this.noticias = response.articles;
+//           console.log('Noticias',this.noticias);
+
+
+//           this.noticias.forEach((element, index) => {
+//             index < 10 ? this.noticiasPrincipales.push(element) : this.noticiasSecundarias.push(element);
+//         });
+
+//           // this.noticias.forEach((element, index) => {
+//           //     if(index < 10){
+//           //       this.noticiasPrincipales.push(element)
+//           //     }
+//           //     else{
+//           //       this.noticiasSecundarias.push(element)
+//           //     }
+//           // });
+
+//         },
+//         error: (error) => {
+//           console.error(error);
+//         },
+//         complete: () => {
+//           console.log('Completado');
+//         }
+//       }
+
+//       this.newsService.obtenerNoticias(selecionarCategoria, pais).subscribe(observer$);
+
+
+
+//       // Forma obsoleta de suscribirse a un observable
+
+//       // this.newsService.obtenerNoticias(categoria, pais).subscribe(
+//       //   (response) => {
+//       //     this.noticias = response.articles;
+//       //     console.log(this.noticias);
+//       //   },
+//       //   (error) => {
+//       //     console.error(error);
+//       //   }
+//       // );
+//     }
+
+// }
