f87eff722c232abae966f7825c712e3383950d82
TomyCaballero <tomyC80@gmail.com>
2023-08-28 19:48:45 -0600

    Se agregan cambios

--- src/app/services/loreminsupm.service.ts
+++ src/app/services/loreminsupm.service.ts
@@ -1,17 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoreminsupmService {
 
-  private url = 'https://loripsum.net/api';
+  private url = 'https://baconipsum.com/api/?type=meat-and-filler&paras=5&format=text';
 
   constructor(private http: HttpClient) { }
 
   getLoreminsupm(): Observable<string> {
-    return this.http.get<any>(this.url);
-  }
+    return this.http.get<any>(this.url).pipe(
+      map((response: any) => {
+        const lorem = JSON.parse(response);
+        console.log('Loreminsupm', lorem);
+        
+        return lorem;        
+      })
+  )}
 }
